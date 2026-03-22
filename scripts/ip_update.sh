2c8b69ddbad69998f1cbc65c37e3fb1a99cb096f
Deepak Ranjan <deepak.ranjan11@hotmail.com>
2026-03-22 18:11:25 +0530

    ip_update files updates with log reporting

--- scripts/ip_update.sh
+++ scripts/ip_update.sh
@@ -1,58 +1,73 @@
+
 #!/bin/bash
 
-DEVICE_CONF="secrets/device.conf"
-CAM_CONF="config/cameras.conf"
-LOG="logs/ip_update.log"
+#------------------------------------------
+# Base directory
+#------------------------------------------
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-NETWORK_PREFIX="config/network.conf"
+NETWORK_PREFIX="$BAE_DIR/config/network.conf"
+DEVICE_CONF="$BASE_DIR/secrets/device.conf"
+CAM_CONF="$BASE_DIR/config/cameras.conf"
 
-echo "----------------------------------------" >> "$LOG"
-echo "$(date) - Updating IPs from MAC" >> "$LOG"
+#------------------------------------------
+# Logging setup (shared)
+#------------------------------------------
+LOG_DIR="$BASE_DIR/logs"
+mkdir -p "$LOG_DIR"
 
-# Clear old camera config
-> "$CAM_CONF"
+LOG="$LOG_DIR/recorder_$(date +%F).log"
 
-echo "$(date) - Scanning network..." >> "$LOG"
+# Delete log older than 7 days
+find "$LOG_DIR" -name "recorder_*.log" -mtime +7 -delete
 
-# Scan network to populate ARP
-for i in {1..254}; do
-    ping -c1 -W1 ${NETWORK_PREFIX}.$i >/dev/null 2>&1 &
-done
-wait
-
-sleep 2
-
-# Function: get IP from MAC
-get_ip_from_mac() {
-    local MAC=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-
-    arp -an | while read -r line; do
-        IP=$(echo "$line" | awk '{print $2}' | tr -d '()')
-        ARP_MAC=$(echo "$line" | awk '{print $4}' | tr '[:upper:]' '[:lower:]')
-
-        if [[ "$ARP_MAC" == "$MAC" ]]; then
-            echo "$IP"
-            return
-        fi
-    done
+log() {
+	LEVEL="$1"
+    CAM="$2"
+    EVENT="$3"
+    MSG="$4"
+    
+    echo "$(date '+%F %T') | $LEVEL | $CAM | $EVENT | $MSG" >> "$LOG"
 }
 
-# Read device.conf
-while IFS='=' read -r NAME MAC
-do
-    # Skip empty lines
-    [ -z "$NAME" ] && continue
+log INFO SYSTEM IP_UPDATE "Starting IP updated"
 
-    IP=$(get_ip_from_mac "$MAC")
+#------------------------------------------
+# Populate ARP table
+#------------------------------------------
+log INFO SYSTEM ARP_SCAN "Scanning netwrok"
 
-    if [ -n "$IP" ]; then
-        echo "$NAME $IP" >> "$CAM_CONF"
-        echo "$(date) - $NAME -> $IP" >> "$LOG"
-    else
-        echo "$(date) - $NAME -> NOT FOUND" >> "$LOG"
+for i in {1..254}; do
+	ping -c 1 -w 1 "$NETWORK_PREFIX.$i" >/dev/null 2>&1 &
+done
+
+wait
+
+log INFO SYSTEM ARP_SCAN "Scan complete"
+
+#------------------------------------------
+# Update camera IPs
+#------------------------------------------
+while IFS=',' read -r NAME MAC; do
+
+	MAC_UPPER=$(echo "$MAC" | tr '[:lower:]' '[:upper:]')
+    
+    IP=$(ip neigh | awk -v  mac="$MAC_UPPER" 'tolower($5)==tolower(mac) {print $1}' | head -n1)
+    
+    if [ -z "$IP" ]; then
+    	log ERROR "$NAME" IP_NOT_FOUND "MAC=$MAC"
+        continue
     fi
-
-done < "$DEVICE_CONF"
-
-echo "$(date) - IP update completed" >> "$LOG"
-echo "----------------------------------------" >> "$LOG"
+    
+    OLD_IP=$(grep "^$NAME " "$CAM_CONF" | awk '{print $2}')
+    
+    if [ "$OLD_IP" != "$IP" ]; then
+    	sed -i "s/^$NAME .*/$NAME $IP/" "$CAM_CONF"
+        log WARN "$NAME" IP_CHANGED "Old=$OLD_IP New=$IP"
+    else
+      	log INFO "$NAME" IP_OK "IP=$IP"
+     fi
+     
+  done < "$DEVICE_CONF"
+  
+  log INFO SYSTEM IP_UPDATE "Completed"
